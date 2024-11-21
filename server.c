47e9d5787ae8f61b52e19a97113dd668bb4507b6
0x0703 <gordonmccannm4321@gmail.com>
2024-11-22 01:04:53 +0300

    Update server.c

--- server.c
+++ server.c
@@ -1,77 +1,40 @@
-// Server Code (server.c)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
 #define PORT 8888
+#define BUFFER_SIZE 1024
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    char buffer[BUFFER_SIZE];
 
-    int port = atoi(argv[1]);
-
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
     
-    // Create TCP socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-    
-    // Set socket options
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(port);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
 
-    // Bind socket to port
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    
-    // Listen for incoming connections
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
+    bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    listen(server_socket, 1);
 
-    // Print message that server is listening on the specified port
-    printf("Server is listening on port %d\n", port);
-    
-    while (1) {
-        // Accept incoming connection
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("accept");
-            exit(EXIT_FAILURE);
-        }
+    client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len);
 
-        // Log client connection
-        printf("Client connected: %s:%d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+    read(client_socket, buffer, BUFFER_SIZE);
+    printf("Client: %s\n", buffer);
 
-        // Handle client communication and game logic
+    // Send a message back to the client
+    char *response = "Message received";
+    write(client_socket, response, strlen(response));
 
-        // Close the connection
-        close(new_socket);
+    close(client_socket);
+    close(server_socket);
 
-        // Log client disconnection
-        printf("Client disconnected: %s:%d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-    }
-    
     return 0;
 }
